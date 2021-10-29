9e15fd9989aa70a21bae15a332d9b348b18f12d9
Maxim <mkolodyazhny@gmail.com>
2021-10-29 14:49:33 +0300

    first commit

---
+++ HW8.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+double y_der(double x, double y){
+    return y / (x * x) + exp((x * x - 1) / x);
+}
+
+void Euler(double(deriv)(double, double), double h, double begin, double end, double y){
+    cout << begin << " " << y << endl;
+    for (auto x = begin + h; x < end + h; x+=h){
+        y += h * y_der(x, y);
+        cout << x << " " << y << endl;
+    }
+}
+
+void RungeKutt(double(deriv)(double, double), double h, double begin, double end, double y){
+    double K1, K2, K3, K4;
+    for (auto x = begin; x < end + h; x+=h){
+        cout << x << " " << y << endl;
+        K1 = deriv(begin, y), K2 = deriv(begin + h/2, y + h/2 * K1), 
+        K3 = deriv(begin + h/2, y + h/2 * K2), K4 = deriv(begin + h, y + h * K3);
+        y = y + h/6 * (K1 + 2*K2 + 2*K3 + K4);
+    }
+}
+
+void EulerKoshi(double(deriv)(double, double), double h, double begin, double end, double y){
+    cout << begin << " " << y << endl;
+    for (auto x = begin + h; x < end + h; x+=h){
+        y = y + h/2 * (deriv(x, y) + deriv(x, y + h*deriv(x, y)));
+        cout << x << " " << y << endl;
+    }
+}
+
+int main(){
+    double h = 0.1, begin = 1, end = 2, y = 1.367879;
+    cout << "Euler:" << endl;
+    Euler(y_der, h, begin, end, y);
+    cout << "RungeKutt:" << endl;
+    RungeKutt(y_der, h, begin, end, y);
+    cout << "EulerKoshi:" << endl;
+    EulerKoshi(y_der, h, begin, end, y);
+}
