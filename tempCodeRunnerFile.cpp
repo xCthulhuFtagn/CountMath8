2eefd97fdd8f6af73a7eba48f2efc0c661c29923
Максим Колодяжный <mkolodyazhny@gmail.com>
2021-11-19 13:43:49 +0300

    фыдларДРОюы

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <cmath>
 #include <vector>
@@ -20,6 +19,7 @@ void Euler(double(deriv)(double, double), double h, double begin, double end, do
         y += h * deriv(x, y);
         cout << x << " ** " << y << " ** " << func(x) << endl;
     }
+    cout << "Error is " << fabs(func(end) - y) << endl;
 }
 
 void RungeKutt(double(deriv)(double, double), double h, double begin, double end, double y){
@@ -30,6 +30,7 @@ void RungeKutt(double(deriv)(double, double), double h, double begin, double end
         K3 = deriv(x + h/2, y + h/2 * K2), K4 = deriv(x + h, y + h * K3);
         y = y + h/6 * (K1 + 2*K2 + 2*K3 + K4);
     }
+    cout << "Error is " << fabs(func(end) - y) << endl;
 }
 
 void EulerKoshi(double(deriv)(double, double), double h, double begin, double end, double y){
@@ -38,6 +39,35 @@ void EulerKoshi(double(deriv)(double, double), double h, double begin, double en
         y = y + h * (deriv(x, y) + deriv(x + h, y + h * deriv(x, y))) / 2;
         cout << x+h << " ** " << y << " ** " << func(x+h) << endl;
     }
+    cout << "Error is " << fabs(func(end) - y )<< endl;
+}
+
+void Adams(double(deriv)(double, double), double h, double begin, double end, double y){
+    double K1, K2, K3, K4, error = 0;
+    vector<double> y_, q, q1, q2, q3;
+    y_.push_back(y);
+    cout << begin << " " << *y_.rbegin() << endl;
+    q.push_back(deriv(begin, *y_.rbegin()) * h);
+    for (auto x = begin; x < begin + 3*h; x+=h){
+        K1 = deriv(x, *y_.rbegin()), K2 = deriv(x + h/2, *y_.rbegin() + h/2 * K1), 
+        K3 = deriv(x + h/2, *y_.rbegin() + h/2 * K2), K4 = deriv(x + h, *y_.rbegin() + h * K3);
+        y_.push_back(*y_.rbegin() + h/6 * (K1 + 2*K2 + 2*K3 + K4));
+        cout << x + h << " " << *y_.rbegin() << endl;
+        q.push_back(deriv(x + h, *y_.rbegin()) * h);
+    }
+    for (auto i = 1; i < 4; ++i) q1.push_back(q[i] - q[i - 1]);
+    for (auto i = 1; i < 3; ++i) q2.push_back(q1[i] - q1[i - 1]);
+    q3.push_back(q2[1] - q2[0]);
+    for (auto x = begin + 4*h; x < end + h; x += h)
+    {
+        double dy = *q.rbegin() + *q1.rbegin() * 1.0 / 2 + *q2.rbegin() * 5.0 / 12 + *q3.rbegin() * 3.0 / 8;        y_.push_back(*y_.rbegin() + dy);
+        cout << x << " ** " << *y_.rbegin() << " ** " << func(x) << endl;
+        q.push_back(deriv(x, *y_.rbegin()) * h);
+        q1.push_back(*(q.rbegin()) - *(q.rbegin() + 1));
+        q2.push_back(*(q1.rbegin()) - *(q1.rbegin() + 1));
+        q3.push_back(*(q2.rbegin()) - *(q2.rbegin() + 1));
+    }
+    cout << "Error is " << fabs(func(end) - *y_.rbegin()) << endl;
 }
 
 int main(){
@@ -48,8 +78,7 @@ int main(){
     RungeKutt(y_der, h, begin, end, y);
     cout << "x ** EulerKoshi ** function" << endl;
     EulerKoshi(y_der, h, begin, end, y);
+    cout << "x ** Adams ** function" << endl;
+    Adams(y_der, h, begin, end, y);
+
 }
-=======
-q.push_back(deriv(x, *y_.rbegin()) * h);
-        
->>>>>>> 906f0623794eb2e73b686775dbad3f4624a3c553
